1ea1b296c66cea3a4b56868d0e11351224ed6ec2
Horizon489732 <tranminhhien27102003@gmail.com>
2025-09-22 16:09:45 -0400

    done with Hero Section

--- frontend/src/sections/Hero.tsx
+++ frontend/src/sections/Hero.tsx
@@ -7,21 +7,30 @@ import Button from "@/components/Button";
 const Hero: FC = () => {
   return (
     <section>
-      <div className="container !max-w-full">
-        <h1 className="text-5xl mt-40">From Equations to Applications — Building Systems That Work</h1>
-        <div>
-          <Button variant="secondary" iconAfter={
-            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-6">
-              <path strokeLinecap="round" strokeLinejoin="round" d="m4.5 5.25 7.5 7.5 7.5-7.5m-15 6 7.5 7.5 7.5-7.5" />
-            </svg>
-          }>
-            <span>View My Work</span>
-          </Button>
-          <Button variant="text">Let&apos;s Talk</Button>
-        </div>
-      </div>
-      <div>
-        <Image src = {heroImage} alt = "My portrait" />
+      <div className="grid md:grid-cols-12 md:h-screen items-stretch">
+
+          <div className="md:col-span-7 flex flex-col justify-center">
+                <div className="container !max-w-full">
+                  <h1 className="text-5xl md:text-6xl mt-40 md:mt-0">From Equations to Applications — Building Systems That Work</h1>
+                  <div className="flex flex-col md:flex-row md:items-center mt-10 items-start gap-6">
+                    <Button variant="secondary" iconAfter={
+                      <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" className="size-5">
+                        <path strokeLinecap="round" strokeLinejoin="round" d="m4.5 5.25 7.5 7.5 7.5-7.5m-15 6 7.5 7.5 7.5-7.5" />
+                      </svg>
+                    }>
+                      <span>View My Work</span>
+                    </Button>
+                    <Button variant="text">Let&apos;s Talk</Button>
+                  </div>
+                </div>
+          </div>
+
+          <div className="md:col-span-5">
+            <div className="h-full inset-0 mt-20 md:mt-0">
+              <Image src = {heroImage} alt = "My portrait" className="size-full object-cover filter grayscale"/>
+            </div>
+          </div>
+                  
       </div>
     </section>
   );
