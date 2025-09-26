cdc55a72c772a9e2d20e78cf744be9ca4a262997
Horizon489732 <tranminhhien27102003@gmail.com>
2025-09-26 10:35:25 -0400

    added animation for theory popup

--- frontend/src/components/TheoryDetails.tsx
+++ frontend/src/components/TheoryDetails.tsx
@@ -1,4 +1,5 @@
 import { HTMLAttributes } from "react";
+import { motion } from "motion/react";
 import Card from "./Card";
 
 
@@ -16,14 +17,17 @@ const TheoryDetails = (
 
     return(
         <div className="fixed isolate inset-0 z-50 flex items-center justify-center w-full h-full overflow-hidden backdrop-blur-sm">
-            <Card>
-                <button onClick={closeFunc}>Close</button>
-
-                <div>
-                    <h5 className="mb-2 text-2xl font-bold text-white">{project.title}</h5>
-                    <p className="mb-3 font-normal text-secondary-dark">{project.description}</p>
-                </div>
-            </Card>
+            <motion.div
+                initial={{opacity: 0, scale: 0.5}}
+                animate={{opacity: 1, scale: 1}}>
+                <Card>
+                    <button onClick={closeFunc}>Close</button>
+                    <div>
+                        <h5 className="mb-2 text-2xl font-bold text-white">{project.title}</h5>
+                        <p className="mb-3 font-normal text-secondary-dark">{project.description}</p>
+                    </div>
+                </Card>
+            </motion.div>
         </div>
     )
 }
