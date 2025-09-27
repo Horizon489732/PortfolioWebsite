ab2cd501df08b9a8ffb3a578f4b244dc703caaed
Horizon489732 <tranminhhien27102003@gmail.com>
2025-09-26 22:32:58 -0400

    Spin baby spin

--- frontend/src/sections/Experience.tsx
+++ frontend/src/sections/Experience.tsx
@@ -46,6 +46,7 @@ const Experience: FC = () => {
     offset: ["start end", "end 72%"],
   });
 
+  const fanRotate = useTransform(scrollYProgress, [0, 1], [0, 3600]);
 
   return (
     <section id="experience" className="pb-16 lg:py-24 bg-neutral-light">
@@ -66,7 +67,17 @@ const Experience: FC = () => {
               whileInView="visible"
               viewport={{ once: true, amount: 0.2 }}
               className="p-6 bg-neutral-dark rounded-2xl shadow-sm hover:shadow-md transition-shadow">
-              <div className="flex flex-col gap-3 md:flex-row md:items-end md:justify-between mb-4 md:mb-5">
+              <div className="flex flex-col gap-3 md:flex-row md:items-end md:justify-between mb-4 md:mb-5 relative">
+                <motion.svg
+                  style={{ rotate: fanRotate }}
+                  className="absolute top-1 right-1 w-5 h-5 md:w-8 md:h-8 text-primary"
+                  viewBox="0 0 24 24"
+                  fill="currentColor"
+                  xmlns="http://www.w3.org/2000/svg"
+                >
+                  <rect width="24" height="24" fill="none" />
+                  <path d="M12,11a1,1,0,1,0,1,1,1,1,0,0,0-1-1m.5-9C17,2,17.1,5.57,14.73,6.75a3.36,3.36,0,0,0-1.62,2.47,3.17,3.17,0,0,1,1.23.91C18,8.13,22,8.92,22,12.5c0,4.5-3.58,4.6-4.75,2.23a3.44,3.44,0,0,0-2.5-1.62,3.24,3.24,0,0,1-.91,1.23c2,3.69,1.2,7.66-2.38,7.66C7,22,6.89,18.42,9.26,17.24a3.46,3.46,0,0,0,1.62-2.45,3,3,0,0,1-1.25-.92C5.94,15.85,2,15.07,2,11.5,2,7,5.54,6.89,6.72,9.26A3.39,3.39,0,0,0,9.2,10.87a2.91,2.91,0,0,1,.92-1.22C8.13,6,8.92,2,12.48,2Z"/>
+                </motion.svg>
                 <h3 className="text-2xl md:text-4xl mt-2 md:mt-5">{exp.role}</h3>
                 <span className="text-sm text-primary-light">{exp.date}</span>
               </div>
