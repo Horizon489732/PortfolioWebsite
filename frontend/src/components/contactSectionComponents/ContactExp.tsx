f5df3d69a49a1b745111ab81d078f5f06537d063
Horizon489732 <tranminhhien27102003@gmail.com>
2025-09-30 17:43:06 -0400

    small fixes for mobile view

--- frontend/src/components/contactSectionComponents/ContactExp.tsx
+++ frontend/src/components/contactSectionComponents/ContactExp.tsx
@@ -8,7 +8,7 @@ const ContactExp: FC = () => {
     return(
         <Canvas camera={{ position: [-1.5, 1.5, 5] }}>
             <ambientLight intensity={2} />
-            <directionalLight position={[-5, 5, 5]} intensity={5} color={"#1C34FF"} />
+            <directionalLight position={[-5, 5, 5]} intensity={5} color={"#804000"} />
             <group rotation={[0, -0.5, 0]} position={[-0.7, -0.5, 0]} scale={2}>
                 <ThreeDModel />
             </group>
